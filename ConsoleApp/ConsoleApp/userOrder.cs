ffc4e4f1d9dbf80b66057cf8890d26cd60f70eac
MarwaSamy <marwasamy0998@gmail.com>
2023-09-18 22:55:30 +0300

    add versions for app

--- ConsoleApp/ConsoleApp/userOrder.cs
+++ ConsoleApp/ConsoleApp/userOrder.cs
@@ -1,13 +1,12 @@
 using System.Reflection;
-
+[assembly: AssemblyVersion("2.0.0.0")]
 public class userOrder
 {
 	Order order = new Order ();
 	public void Display()
 	{
-		System.Console.WriteLine (order.ToString ());
-        System.Console.WriteLine("V 2");
-
+        System.Console.WriteLine("V2:");
+        System.Console.WriteLine (order.ToString ());
     }
 
 
