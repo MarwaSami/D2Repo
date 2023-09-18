246e0fb6c0d03e8a0235ca5ab31535e2ac5a9148
Mohamed <mo7amedelmostafa@gmail.com>
2023-09-18 14:41:22 +0300

    Order

--- ConsoleApp/ConsoleApp/Order.cs
+++ ConsoleApp/ConsoleApp/Order.cs
@@ -1,11 +1,11 @@
-﻿public class Order ()
+﻿public class Order
 {
     public int OrderID { get; set; }
     public string OrderName { get; set; }
     public string OrderDate { get; set; }
     public string OrderType { get; set; }
-    User user { get; set; }
-  public override string ToString()
+    public User user { get; set; }
+    public override string ToString()
     {
         return $"{OrderID}, {OrderName} ";
     }
