e388ad0dd9d0d21262d35f5625e7485d682396a1
MarwaSamy <marwasamy0998@gmail.com>
2023-09-18 14:58:16 +0300

    add app

---
+++ ConsoleApp/ConsoleApp/App.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    public class App
+    {
+      public static void Main(string[] args) {
+        userOrder userOrder = new userOrder();
+        userOrder.Display();
+    }
+   }
+
